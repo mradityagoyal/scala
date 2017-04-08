d759982c69a9ca0d8552fee9d9f1218b9066e8c4
Aditya Goyal <aditya.g.goyal@oracle.com>
2017-04-08 17:14:36 -0400

    dont override main method

--- AlgosStandordCoursera/app/com/coursera/stanford/algorithms/wk1/Main.scala
+++ AlgosStandordCoursera/app/com/coursera/stanford/algorithms/wk1/Main.scala
@@ -2,7 +2,6 @@ package com.coursera.stanford.algorithms.wk1
 
 object Main extends App{
   
-  override def main(args: Array[String]) = {
 //    val res = Multiplication.recursive_multiply_base10(1234,5678)
 //    val res = KaratsubaMultiplication.recursive_multiply(1234, 5678)
 //    println(s"$res")  
@@ -12,6 +11,5 @@ object Main extends App{
 //    println(Sorting.minAndRest(input))
     val sorted = Sorting.minAndRest(Vector(1,2))
     print(sorted)
-  }
   
 }
