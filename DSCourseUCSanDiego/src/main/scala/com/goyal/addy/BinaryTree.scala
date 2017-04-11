ec3b27d3a40a54783dcc2a700685152264d77928
Aditya Goyal <aditya.g.goyal@oracle.com>
2017-04-11 10:35:29 -0400

    add popMin, min, max methods

--- DSCourseUCSanDiego/src/main/scala/com/goyal/addy/BinaryTree.scala
+++ DSCourseUCSanDiego/src/main/scala/com/goyal/addy/BinaryTree.scala
@@ -1,64 +1,80 @@
 package com.goyal.addy
 
-case class Node[A](key: A, left: Option[Node[A]] = None, right: Option[Node[A]] = None) {
+import scala.annotation.tailrec
+import scala.util.control.TailCalls.TailRec
+
+case class BinaryTree[T <% Ordered[T]](key: T, left: Option[BinaryTree[T]] = None, right: Option[BinaryTree[T]] = None) {
+  require(key != Nil, "Can't have Nil key") //TODO: check if required
+  require(left match {
+    case None => true
+    case Some(l) => l.key <= key
+  }, "Left can't be more than key")
+
+  require(right match {
+    case None => true
+    case Some(r) => r.key > key
+  }, "Right can't be less than key")
 
   lazy val height: Int = 1 + Math.max(left.map(_.height).getOrElse(0), right.map(_.height).getOrElse(0))
 
   lazy val size: Int = 1 + left.map(_.size).getOrElse(0) + right.map(_.size).getOrElse(0)
 
-  def nodesAtHeight(ht: Int): List[Node[A]] = {
+  lazy val min: T = left.map(_.min).getOrElse(key)
+
+  lazy val max: T = left.map(_.max).getOrElse(key)
+
+  def add(x: T): BinaryTree[T] = {
+    if (x <= key) {
+      lazy val newLeft = (left.map(_.add(x))).getOrElse(BinaryTree(x))
+      BinaryTree(key, Some(newLeft), right)
+    } else {
+      lazy val newRight = (right.map(_.add(x))).getOrElse(BinaryTree(x))
+      BinaryTree(key, left, Some(newRight))
+    }
+  }
+
+  /**
+   * pops and returns ths min and rest.
+   */
+  def popMin: (T, Option[BinaryTree[T]]) = left match {
+    case None => (key, right) // if left is None 
+    case Some(lv) => {
+      val (min, newLeft) = lv.popMin
+      (min, Some(BinaryTree(key, newLeft, right)))
+    }
+  }
+
+  def nodesAtHeight(ht: Int): List[BinaryTree[T]] = {
     require(ht > 0, "Cant specify ht less than 1")
     if (ht == 1) {
       List(this)
     } else {
-      val empt = List.empty[Node[A]]
+      val empt = List.empty[BinaryTree[T]]
       left.map(_.nodesAtHeight(ht - 1)).getOrElse(empt) ::: right.map(_.nodesAtHeight(ht - 1)).getOrElse(empt)
     }
   }
 
-  def rowN(n: Int): String = {
-    if (n == 1) {
-      key.toString()
-    } else {
-      left.map(_.rowN(n - 1)).getOrElse("\t") + "," + right.map(_.rowN(n - 1)).getOrElse("\t")
-    }
-  }
-
   def stringify: List[String] = {
-
-    //h number of lines will be there.
     val tab = "\t"
-
-    val line1 = tab * (height - 1) + key
-
-    def nextLvl(in: List[Option[Node[A]]]): List[Option[Node[A]]] = {
+    def nextLvl(in: List[Option[BinaryTree[T]]]): List[Option[BinaryTree[T]]] = {
       in.flatMap(_ match {
-        case None => List[Option[Node[A]]](None, None) //TODO: fix
+        case None => List[Option[BinaryTree[T]]](None, None) //TODO: fix
         case Some(nd) => List(nd.left, nd.right)
       })
     }
-
-    def rowToStr(in: List[Option[Node[A]]]): String = in.map(_ match {
+    def lvlToStr(in: List[Option[BinaryTree[T]]]): String = in.map(_ match {
       case None => "None"
       case Some(x) => x.key.toString()
-    }).reduce(_ +" , " + _)
+    }).reduce(_ + " , " + _)
 
-    val top = List(Some(this))
-    val l2 = nextLvl(top)
-    val l2s = rowToStr(l2)
-    
-    val rowsToReturn : List[String] = List.empty
-    
-    rowToStr(top)
-    
-    def recur(levelsLeft: Int, acc: List[String], thisLevel: List[Option[Node[A]]]): List[String] = {
-      if(levelsLeft == 1){
-        acc :+ rowToStr(thisLevel)
-      }else {
-        recur(levelsLeft -1, acc :+ (("\t"*(levelsLeft -1)) + rowToStr(thisLevel)), nextLvl(thisLevel))
+    @tailrec
+    def recur(levelsLeft: Int, acc: List[String], thisLevel: List[Option[BinaryTree[T]]]): List[String] = {
+      if (levelsLeft == 1) {
+        acc :+ lvlToStr(thisLevel)
+      } else {
+        recur(levelsLeft - 1, acc :+ (("\t" * (levelsLeft - 1)) + lvlToStr(thisLevel)), nextLvl(thisLevel))
       }
     }
-
     recur(height, List.empty[String], List(Some(this)))
   }
 
--- DSCourseUCSanDiego/src/test/scala/com/goyal/addy/BinaryTreeSpec.scala
+++ DSCourseUCSanDiego/src/test/scala/com/goyal/addy/BinaryTreeSpec.scala
@@ -4,33 +4,35 @@ import org.scalatest.FlatSpec
 import org.scalatest.Matchers
 import org.scalatest.prop.PropertyChecks
 
-class BinaryTreeSpec extends FlatSpec with Matchers with PropertyChecks{
-  
+class BinaryTreeSpec extends FlatSpec with Matchers with PropertyChecks {
+
   "A Node's height operation " should "behave correctly " in {
-    val leaf = com.goyal.addy.Node("world")
+    val leaf = com.goyal.addy.BinaryTree("WWWW")
     assert(leaf.height === 1)
-    val root = Node("Hello", Some(Node("blah")), Some(Node("world")))
+    val root = BinaryTree("PPPP", Some(BinaryTree("AAAA")), Some(BinaryTree("WWWW")))
     assert(root.height === 2)
-    assert(Node("hello", Some(root), None).height === 3)
+    val error = intercept[IllegalArgumentException] {
+      val illegal = BinaryTree("AAAA", Some(BinaryTree("BBBB")), None)
+    }
+    assert(!error.getMessage.isEmpty())
   }
-  
+
   it should "have correct size method" in {
-    val leaf = com.goyal.addy.Node("world")
-    leaf.size should === (1)
-    val sz3 = Node("Hello", Some(Node("blah")), Some(Node("world")))
-    sz3.size should === (3)
-    Node("hello", Some(sz3), None).size should === (4)
+    val leaf = com.goyal.addy.BinaryTree("world")
+    leaf.size should ===(1)
+    val sz3 = leaf.add("Hello").add("123")
+    sz3.size should ===(3)
+    sz3.add("3421").size should ===(4)
   }
-  
+
+  //TODO: ignored for now due to comparison check not implemented. 
   it should "have correct nodesAtHeight() method" in {
-    val (leaf, blah, wrld)  = (Node("Hello"), Node("blah"), Node ("world"))
-    leaf.nodesAtHeight(1) should === (List(leaf))
-    val sz3 = Node("Hello", Some(blah), Some(wrld))
-    sz3.nodesAtHeight(2) should === (List(blah, wrld))
-    Node("Hello", Some(blah), None).nodesAtHeight(2) should === (List(blah))
-    sz3.nodesAtHeight(3).isEmpty should === (true)
+    val (leaf, blah, wrld) = (BinaryTree("Hello"), BinaryTree("blah"), BinaryTree("world"))
+    leaf.nodesAtHeight(1) should ===(List(leaf))
+    val sz3 = BinaryTree("Hello").add("some").add("other")
+    sz3.nodesAtHeight(2) should ===(List(blah, wrld))
+    BinaryTree("Hello", Some(blah), None).nodesAtHeight(2) should ===(List(blah))
+    sz3.nodesAtHeight(3).isEmpty should ===(true)
   }
-  
-  
-  
+
 }
