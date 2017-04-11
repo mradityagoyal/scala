e50e6ee7b12fe2bc049d3f1c02801fe88e50c649
Aditya Goyal <aditya.g.goyal@oracle.com>
2017-04-11 08:38:58 -0400

    implement stringify on Node

--- DSCourseUCSanDiego/src/main/scala/com/goyal/addy/BinaryTree.scala
+++ DSCourseUCSanDiego/src/main/scala/com/goyal/addy/BinaryTree.scala
@@ -24,4 +24,42 @@ case class Node[A](key: A, left: Option[Node[A]] = None, right: Option[Node[A]] 
     }
   }
 
+  def stringify: List[String] = {
+
+    //h number of lines will be there.
+    val tab = "\t"
+
+    val line1 = tab * (height - 1) + key
+
+    def nextLvl(in: List[Option[Node[A]]]): List[Option[Node[A]]] = {
+      in.flatMap(_ match {
+        case None => List[Option[Node[A]]](None, None) //TODO: fix
+        case Some(nd) => List(nd.left, nd.right)
+      })
+    }
+
+    def rowToStr(in: List[Option[Node[A]]]): String = in.map(_ match {
+      case None => "None"
+      case Some(x) => x.key.toString()
+    }).reduce(_ +" , " + _)
+
+    val top = List(Some(this))
+    val l2 = nextLvl(top)
+    val l2s = rowToStr(l2)
+    
+    val rowsToReturn : List[String] = List.empty
+    
+    rowToStr(top)
+    
+    def recur(levelsLeft: Int, acc: List[String], thisLevel: List[Option[Node[A]]]): List[String] = {
+      if(levelsLeft == 1){
+        acc :+ rowToStr(thisLevel)
+      }else {
+        recur(levelsLeft -1, acc :+ (("\t"*(levelsLeft -1)) + rowToStr(thisLevel)), nextLvl(thisLevel))
+      }
+    }
+
+    recur(height, List.empty[String], List(Some(this)))
+  }
+
 }
