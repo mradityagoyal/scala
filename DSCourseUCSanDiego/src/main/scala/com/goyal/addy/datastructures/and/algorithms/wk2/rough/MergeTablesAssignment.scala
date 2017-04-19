04c54d5e422ac85e31eeebd67675fe6e0bde1e6a
Aditya Goyal <Aditya.G.Goyal@oracle.com>
2017-04-19 16:53:25 -0400

    initial try at merge tables assignment 2 q 3

---
+++ DSCourseUCSanDiego/src/main/scala/com/goyal/addy/datastructures/and/algorithms/wk2/rough/MergeTablesAssignment.scala
@@ -0,0 +1,48 @@
+package com.goyal.addy.datastructures.and.algorithms.wk2.rough
+
+import scala.io.StdIn
+import scala.annotation.tailrec
+
+object MergeTablesAssignment extends App{
+  
+  val nm = StdIn.readLine().split(" ").map(_.toInt)
+  val n = nm(0)
+  val m = nm(1)
+
+  val counts: Array[Long] = StdIn.readLine().split(" ").map(_.toLong)
+
+  val parents: Array[Int] = (0 until n).toArray
+  var maxCount = counts.max
+  
+  for (i <- 0 until m) {
+    val destAndSrc = StdIn.readLine().split(" ").map(_.toInt)
+    val (dest, src) = (destAndSrc(0) -1, destAndSrc(1) -1)
+    merge(dest , src )
+    print(maxCount)
+  }
+  
+  /** Returns the id of the root of node at this id. */
+  @tailrec
+  def getRootIdx(i: Int): Int = {
+    if (parents(i) == i) i
+    else getRootIdx(parents(i))
+  }
+  
+  /**merges the src into destination. */
+  def merge(dest: Int, src: Int): Unit = {
+    val destRoot = getRootIdx(dest)
+    val srcRoot = getRootIdx(src)
+    if (destRoot != srcRoot) {
+      //copy rows from src to dest. 
+      val srcCount: Long = counts(srcRoot)
+      val destCount: Long = counts(destRoot)
+      val combinedCount = srcCount + destCount
+      //store max number of rows. 
+      if (maxCount < combinedCount) maxCount = combinedCount
+      counts(destRoot) = combinedCount
+      //set parent of srcRoot = destRoot. (merge trees)
+      parents(srcRoot) = destRoot
+    }
+  }
+  
+}
