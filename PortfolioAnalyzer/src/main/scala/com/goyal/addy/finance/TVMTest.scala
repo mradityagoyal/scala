e92db65e853df6180fcf66196b13e624f3f519e3
Aditya Goyal <Aditya.G.Goyal@oracle.com>
2017-05-17 16:04:32 -0400

    TVM initial future value and irr

--- PortfolioAnalyzer/src/main/scala/com/goyal/addy/finance/TVMTest.scala
+++ PortfolioAnalyzer/src/main/scala/com/goyal/addy/finance/TVMTest.scala
@@ -1,8 +1,25 @@
 package com.goyal.addy.finance
 
+import java.time.{Instant, LocalDateTime, ZoneOffset}
+import javafx.scene.control.SpinnerValueFactory.LocalDateSpinnerValueFactory
+
+import com.goyal.addy.finance.TimeValueMoney.CashFlowEvent
+
 /**
   * Created by agoyal on 5/17/17.
   */
-class TVMTest {
+object TVMTest extends App{
+
+  val cashFlows = List(CashFlowEvent(100, Instant.parse("2016-05-17T10:15:30.00Z")))
+  val t1 = TimeValueMoney.fv(cashFlows, LocalDateTime.now().toInstant(ZoneOffset.UTC), 0.05)
+ // print(t1)
+
+
+  val r = TimeValueMoney.irr(105, cashFlows)
+
+  println(r)
+
+
+//  val
 
 }
--- PortfolioAnalyzer/src/main/scala/com/goyal/addy/finance/TimeValueMoney.scala
+++ PortfolioAnalyzer/src/main/scala/com/goyal/addy/finance/TimeValueMoney.scala
@@ -1,8 +1,44 @@
 package com.goyal.addy.finance
 
+import java.time.{Duration, Instant, LocalDateTime, Period}
+
+
 /**
   * Created by agoyal on 5/17/17.
   */
-class TimeValueMoney {
+object TimeValueMoney {
+
+  case class CashFlowEvent(amount: Double, time: Instant)
+
+
+  /**
+    *
+    * @param cashFlows the list of cash flow envents.
+    * @param futureDate the Instant in future
+    * @param r rate of return.
+    * @return future value of cashflows @ time = futureDate, and @ rate = r
+    */
+  def fv(cashFlows: List[CashFlowEvent], futureDate: Instant, r: Double): Double = {
+    cashFlows.par.map(evt => {
+      require(futureDate.isAfter(evt.time)) //doesnt work when cash flow continues after the future date.
+      val n = Duration.between(evt.time, futureDate).toDays
+      val discFactor: Double = scala.math.pow(1 + r, n / 365)
+      evt.amount * discFactor
+    }).sum
+  }
+
+
+  def irr(fv: Double, cashFlow: List[CashFlowEvent]): Double = {
+    val acceptableError = 0.1
+    var guess = 0.03
+    var calculatedVal = 0d
+    var delta = 0d
+    do {
+      guess = guess + 0.005
+      calculatedVal = TimeValueMoney.fv(cashFlow, Instant.now(), guess)
+      delta = (fv -calculatedVal)
+    }while(delta > acceptableError)
+    guess
+  }
 
 }
