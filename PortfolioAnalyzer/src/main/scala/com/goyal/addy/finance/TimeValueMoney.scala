16dd121d0cf1559a590b176c8fea08d56adcc62d
Aditya Goyal <Aditya.G.Goyal@oracle.com>
2017-05-17 16:15:36 -0400

    add two fv methods

--- PortfolioAnalyzer/src/main/scala/com/goyal/addy/finance/TimeValueMoney.scala
+++ PortfolioAnalyzer/src/main/scala/com/goyal/addy/finance/TimeValueMoney.scala
@@ -19,24 +19,31 @@ object TimeValueMoney {
     * @return future value of cashflows @ time = futureDate, and @ rate = r
     */
   def fv(cashFlows: List[CashFlowEvent], futureDate: Instant, r: Double): Double = {
-    cashFlows.par.map(evt => {
-      require(futureDate.isAfter(evt.time)) //doesnt work when cash flow continues after the future date.
-      val n = Duration.between(evt.time, futureDate).toDays
-      val discFactor: Double = scala.math.pow(1 + r, n / 365)
-      evt.amount * discFactor
-    }).sum
+    cashFlows.par.map(fv(_, futureDate, r)).sum
+  }
+
+  /**
+    *
+    * @param evt The cash flow event.
+    * @param futureDate. the future date.
+    * @param r rate of return.
+    * @return future value of amount invested at rate r.(compounded every 365 days)
+    */
+  def fv(evt: CashFlowEvent, futureDate: Instant, r: Double): Double = {
+    require(futureDate.isAfter(evt.time))
+    val numDays = Duration.between(evt.time, futureDate)
+    val discountFactor = scala.math.pow(1+r, numDays/365)
+    evt.amount * discountFactor
   }
 
 
-  def irr(fv: Double, cashFlow: List[CashFlowEvent]): Double = {
+  def irr(presentValue: Double, cashFlow: List[CashFlowEvent]): Double = {
     val acceptableError = 0.1
     var guess = 0.03
-    var calculatedVal = 0d
     var delta = 0d
     do {
       guess = guess + 0.005
-      calculatedVal = TimeValueMoney.fv(cashFlow, Instant.now(), guess)
-      delta = (fv -calculatedVal)
+      delta = (presentValue - fv(cashFlow, Instant.now(), guess))
     }while(delta > acceptableError)
     guess
   }
