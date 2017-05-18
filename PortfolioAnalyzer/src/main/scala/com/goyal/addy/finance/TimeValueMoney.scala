a3315a24034c8b009e25c0ee8a272056eab6a045
Aditya <mradityagoyal@gmail.com>
2017-05-17 21:25:02 -0400

    Second try at IRR

--- PortfolioAnalyzer/src/main/scala/com/goyal/addy/finance/TimeValueMoney.scala
+++ PortfolioAnalyzer/src/main/scala/com/goyal/addy/finance/TimeValueMoney.scala
@@ -38,14 +38,40 @@ object TimeValueMoney {
 
 
   def irr(presentValue: Double, cashFlow: List[CashFlowEvent]): Double = {
-    val acceptableError = 0.1
-    var guess = 0.03
-    var delta = 0d
-    do {
-      guess = guess + 0.005
-      delta = (presentValue - fv(cashFlow, Instant.now(), guess))
-    }while(delta > acceptableError)
-    guess
+//    val acceptableError = 0.1
+//    var guess = 0.03
+//    var delta = 0d
+//    do {
+//      guess = guess + 0.005
+//      delta = (presentValue - fv(cashFlow, Instant.now(), guess))
+//    }while(delta > acceptableError)
+//    guess
+
+    var minRate : Option[Double] = None
+    var maxRate : Option[Double] = None
+    val costBasis : Double = cashFlow.map(_.amount).sum
+//    if(presentValue <= costBasis){
+//      maxRate = Some(0)
+//      assert(presentValue >= 0) //cant have present value as negetive.
+//      minRate = Some(-1) //can't have present value as negetive.
+//    }else{
+//      minRate = Some(0)
+//    }
+
+    var guess: Double = if(presentValue <= costBasis) -0.01 else 0.01
+
+    val calculatedValue = fv(cashFlow, Instant.now(), guess)
+
+    if(calculatedValue > presentValue){
+      // guess was too large. increase rate
+      if(!maxRate.isDefined || maxRate.get > guess) maxRate = Some(guess)
+    }else{
+      //guess was too small. decrese rate
+      if(!minRate.isDefined || minRate.get < guess) minRate = Some(guess)
+    }
+
+    0.0 //TODO Remove once implemented
+
   }
 
 }
