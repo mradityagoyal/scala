c08774a3cd4c91967dc9c93ea41be2a373ff4d78
Aditya Goyal <mradityagoyal@gmail.com>
2018-04-27 12:09:39 -0400

    add currency formatting

--- WebPortfolioAnalyzer2/app/services/finance/PortfolioAnalyzer.scala
+++ WebPortfolioAnalyzer2/app/services/finance/PortfolioAnalyzer.scala
@@ -1,5 +1,6 @@
 package services.finance
 
+import java.text.NumberFormat
 import java.time.{Duration, Instant}
 
 import model.{F01KTransaction, FidelityTransaction, IRATransaction}
@@ -19,9 +20,9 @@ object PortfolioAnalyzer extends App{
   val transactionsAddy401: List[FidelityTransaction] = F01KTransaction.fromFile(pathAddy401) //noMore transactions here.
   val transactionsAddy = transactionsAddy401 ::: transactionsAddyIRA
 
-  val addyRothPresentValue: Double = 18112.96
-  val addyIRAPresentValue: Double = 29222.22
-  val ragsRothPresentValue: Double = 5809.92
+  val addyRothPresentValue: Double = 18058
+  val addyIRAPresentValue: Double = 29183
+  val ragsRothPresentValue: Double = 5787
 
   //TODO put the current value of the Portfolio.
   val presentValue : Double = addyRothPresentValue + addyIRAPresentValue + ragsRothPresentValue
@@ -37,22 +38,24 @@ object PortfolioAnalyzer extends App{
 
   println(s"num contributions: ${contributions.size}")
 
-  println(s"total Rags Contributions: ${transactionsRags.filter(contributionFilter).map(t => t.amount.getOrElse(0.0)).sum}")
-  println(s"total Addy Contributions: ${transactionsAddy.filter(contributionFilter).map(t => t.amount.getOrElse(0.0)).sum}")
-  println(s"Addy IRA Contributions: ${transactionsAddyIRA.filter(contributionFilter).map(t => t.amount.getOrElse(0.0)).sum} , (does not include ROLLOVER). ")
-  println(s"Addy 401K Contributions: ${transactionsAddy401.filter(contributionFilter).map(t => t.amount.getOrElse(0.0)).sum} ")
+  val formatter: NumberFormat = NumberFormat.getCurrencyInstance
+
+  println(s"total Rags Contributions: ${formatter.format(transactionsRags.filter(contributionFilter).map(t => t.amount.getOrElse(0.0)).sum)}")
+  println(s"total Addy Contributions: ${formatter.format(transactionsAddy.filter(contributionFilter).map(t => t.amount.getOrElse(0.0)).sum)}")
+  println(s"Addy IRA Contributions: ${formatter.format(transactionsAddyIRA.filter(contributionFilter).map(t => t.amount.getOrElse(0.0)).sum)} , (does not include ROLLOVER). ")
+  println(s"Addy 401K Contributions: ${formatter.format(transactionsAddy401.filter(contributionFilter).map(t => t.amount.getOrElse(0.0)).sum)} ")
   val totalContribution = contributions.map(t => t.amount.getOrElse(0.0)).sum
-  println(s"Total contributions: $totalContribution")
+  println(s"Total contributions: ${formatter.format(totalContribution)}")
 
   val totalDividend = transactions.filter(dividendFilder).map(t => t.amount.getOrElse(0.0)).sum
 
-  println(s"Total dividend = $totalDividend")
+  println(s"Total dividend = ${formatter.format(totalDividend)}")
 
 
 
-  println(s"present value: $presentValue")
+  println(s"present value: ${formatter.format(presentValue)}")
 
-  println(s"total Gain or Loss: ${presentValue - totalContribution}")
+  println(s"total Gain or Loss: ${formatter.format(presentValue - totalContribution)}")
   println(s"total gain percentage : ${((presentValue - totalContribution) / totalContribution)*100}%")
 
   val cashFlow: List[CashFlowEvent]= contributions.map(CashFlowEvent.fromFidelityTransaction)
@@ -63,11 +66,11 @@ object PortfolioAnalyzer extends App{
   val nextYr = Instant.now().plus(Duration.ofDays(365))
   val irrNextYr = TimeValueMoney.irr(presentValue, cashFlow, nextYr)
 
-  println(s"The irr if same present value stayed for a year is ${irrNextYr*100}%")
-  println(s"Portfolio value if you maintain ${irr*100}% return for one more year ${presentValue * (1+irr)}")
+  println(s"The irr if same present value stayed for a year is ${formatter.format(irrNextYr*100)}%")
+  println(s"Portfolio value if you maintain ${irr*100}% return for one more year ${formatter.format(presentValue * (1+irr))}")
 
-  println(s"Dream on: if you maintain this rate of return for 5 years you will have ${presentValue * Math.pow((1+irr), 5)}")
-  println(s"No way this will happen: if you maintain this rate of return for 10 years you will have ${presentValue * Math.pow((1+irr), 10)}")
+  println(s"Dream on: if you maintain this rate of return for 5 years you will have ${formatter.format(presentValue * Math.pow((1+irr), 5))}")
+  println(s"No way this will happen: if you maintain this rate of return for 10 years you will have ${formatter.format(presentValue * Math.pow((1+irr), 10))}")
 
 
 
